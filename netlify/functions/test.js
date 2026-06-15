e76a0bc74b88ff58fd2a9af795398bc2c6536fde
papinoproperties <papinoproperties@gmail.com>
2026-06-15 07:36:05 -0400

    Delete netlify/functions directory

--- netlify/functions/test.js
+++
@@ -1,5 +0,0 @@
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ status: 'ok', message: 'WAJ functions are working', node: process.version })
-});
